767a44e1169ed56b2fa05515fb776d53185a1895
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-13 21:32:37 +0200

    Complete ex_1

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\033[33mU lukomor'a dub zelenij,\n"
+        << "Zlataya zep na dupe tom,\n"
+        << "I dnem i nochju kot uchenij\n"
+        << "vse hodit po cepi krugom\033[0m" << endl;
+    return 0;
+}
+
