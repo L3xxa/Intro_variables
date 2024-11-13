81ac30790e2828bddedcd56ed98444ac38b65639
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-13 20:49:47 +0200

    complete ex_1

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Name: \033[33mThe war and the peace\033[0m" << endl;
+	cout << "Author: Lev Tolstoy" << endl;
+	cout << "Izdatelstvo: Piter" << endl;
+	cout << "Pages: \033[34m500\033[0m" << endl;
+	return 0;
+}
